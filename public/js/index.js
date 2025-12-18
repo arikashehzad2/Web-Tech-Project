63d5955dde83ad7bcc58f584bfccf0ea5a0a279a
Arika Shehzad <arikashehzad2@gmail.com>
2025-12-19 04:02:30 +0500

    Remove unnecessary folders

--- public/js/index.js
+++
@@ -1,8 +0,0 @@
-function toggleContact() {
-  const details = document.getElementById('contact-details');
-  if (details.style.display === 'none') {
-    details.style.display = 'block';
-  } else {
-    details.style.display = 'none';
-  }
-}
